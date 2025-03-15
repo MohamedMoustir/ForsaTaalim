484f9c83c83628c78b16cd9fd73fdf3b4bae3af6
MohamedMoustir <itsmoustir@gmail.com>
2025-03-15 17:01:46 +0000

    Add createProfesseur method to handle professeur creation with file upload

--- forsaTaalim/app/Http/Controllers/ProfesseurController.php
+++ forsaTaalim/app/Http/Controllers/ProfesseurController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Professeur;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Session;
 class ProfesseurController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      */
@@ -18,9 +19,38 @@ class ProfesseurController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function createProfesseur(request $request)
     {
-        //
+        $request->validate([
+            'diplomes' => 'required|string|max:255',
+            'experiences' => 'required|string|max:255',
+            'tarifHoraire' => 'required|string|max:255',
+            'disponible' => 'required|boolean',
+            'location' => 'required|string|max:255',
+            'biographie' => 'required|string',
+            'video' => 'file',
+        ]);
+
+        if ($request->hasFile('video')) {
+            $video = $request->file('video');
+            $videoPath = time() . '_' . $video->getClientOriginalName();
+            $video->move(public_path('videos'), $videoPath);
+
+        }
+        $lastInsertId = Session::get('lastInsertId');
+        $professeur = Professeur::create([
+            'categorieMatiere_id' => intval($lastInsertId),
+            'diplomes' => $request->input('diplomes'),
+            'experiences' => $request->input('experiences'),
+            'tarifHoraire' => $request->input('tarifHoraire'),
+            'disponible' => $request->input('disponible'),
+            'location' => $request->input('location'),
+            'biographie' => $request->input('biographie'),
+            'video' => $videoPath,
+        ]);
+
+        return response()->json(['message' => 'Professeur created successfully!', 'Professeur' => $professeur], 201);
+
     }
 
     /**
