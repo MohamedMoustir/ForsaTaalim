9d2360c75af3bf098131ba53e4100faa978df941
MohamedMoustir <itsmoustir@gmail.com>
2025-03-22 22:18:46 +0000

    Ajout des requêtes de validation pour les annonces

---
+++ forsaTaalim/app/Http/Requests/AnnouncementRequests.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AnnouncementRequests extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // You can perform authorization logic here if needed
+        return true;  // Allow the request
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+           'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'subjects' => 'required|array',
+            'subjects.*' => 'string',
+            'levels' => 'required|array',
+            'levels.*' => 'string',
+            'price' => 'required|numeric',
+            'location' => 'required|string|max:255',
+            'date' => 'required|date',
+            'is_active' => 'required|boolean',
+
+        ];
+    }
+
+}
