05a5e278233dc74b553ef059f9822bc49cc63e3a
MohamedMoustir <itsmoustir@gmail.com>
2025-03-26 15:42:27 +0000

    conecte les pusher avec project

--- forsaTaalim/app/Repositories/ChatRepositories.php
+++ forsaTaalim/app/Repositories/ChatRepositories.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Auth;
 use DB;
 use Hash;
+use Pusher\Pusher;
 use Session;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use function PHPUnit\Framework\returnArgument;
@@ -24,7 +25,25 @@ class ChatRepositories
 
     public function create($data)
     {
-        return $this->model->create($data );
+        // Create the Pusher instance
+        $pusher = new Pusher(
+            env('PUSHER_APP_KEY'),
+            env('PUSHER_APP_SECRET'),
+            env('PUSHER_APP_ID'),
+            [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true
+            ]
+        );
+
+        // Trigger the Pusher event with current timestamp
+        $pusher->trigger('forsaTaalm-development', 'message', [
+            'message' => $data['message'],
+            'sender' => Auth::user()->role,
+            'timestamp' => now()->format('h:i A'),
+        ]);
+
+        return $this->model->create($data);
     }
 
 }
