a8eb7183840395c8057110753256e21c72969bf7
MohamedMoustir <itsmoustir@gmail.com>
2025-03-26 15:43:30 +0000

    int les chatServise

--- forsaTaalim/app/Services/ChatService.php
+++ forsaTaalim/app/Services/ChatService.php
@@ -3,10 +3,12 @@
 namespace App\Services;
 
 use App\Models\CategorieMatiere;
+use App\Models\Chat_user;
 use App\Repositories\CategorieMatiereRepository;
 use App\Interface\CrudInterface;
 use App\Repositories\AuthRepository;
 use App\Repositories\ChatRepositories;
+use Auth;
 
 
 class ChatService 
@@ -19,9 +21,15 @@ class ChatService
     }
 
 
-    public function create(array $data , $id,$sender_id)
+    public function create(array $data , $sender_id ,$id)
     {
-        $data['chat_user_id'] = $id;
+        $chat_user = Chat_user::create([
+            'user_id1' => $sender_id,
+            'user_id2' => $id,
+        ]);
+
+        $chat_user_id = $chat_user->id;
+        $data['chat_user_id'] = $chat_user_id;
         $data['sender_id'] = $sender_id;
 
         return $this->chatRepositories->create($data);
