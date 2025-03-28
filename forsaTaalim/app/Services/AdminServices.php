d44d909b73a2c9d6f80d2b49c80dc5a86f1cf92d
MohamedMoustir <itsmoustir@gmail.com>
2025-03-28 15:16:21 +0000

    add file services por assouer found les donnes

---
+++ forsaTaalim/app/Services/AdminServices.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Announcement;
+use App\Models\CategorieMatiere;
+use App\Repositories\AdminRepositories;
+use App\Repositories\AnnouncementRepositories;
+use App\Repositories\CategorieMatiereRepository;
+use App\Interface\CrudInterface;
+use App\Repositories\AuthRepository;
+use Illuminate\Support\Facades\Log;
+
+class AdminServices implements CrudInterface
+{
+
+    protected $adminRepositories;
+    public function __construct(AdminRepositories $adminRepositories)
+    {
+        $this->adminRepositories = $adminRepositories;
+    }
+    public function create(array $data)
+    {
+        return $this->adminRepositories->create($data);
+    }
+    public function getAll()
+    {
+        return Announcement::all();
+    }
+    public function getById($id)
+    {
+        return Announcement::findOrFail($id);
+    }
+    public function update($id, array $data)
+    {
+        return $this->adminRepositories->update($id, $data);
+    }
+    public function delete($id)
+    {
+        return $this->adminRepositories->delete($id);
+    }
+    public function sospande($id)
+    {
+        return $this->adminRepositories->sospande($id);
+    }
+    public function TotalUser(){
+        return $this->adminRepositories->TotalUser();
+    }
+    public function TotalAnnonce(){
+        return $this->adminRepositories->TotalAnnonce();
+    }
+    public function TotalUserActive(){
+        return $this->adminRepositories->TotalUserActive();
+    } public function TotalUserSospande(){
+        return $this->adminRepositories->TotalUserSospande();
+    }
+}
