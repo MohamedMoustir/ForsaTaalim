33b00815757abf3b8c553d11c30f7e0a4b4eedc7
MohamedMoustir <itsmoustir@gmail.com>
2025-03-29 02:59:35 +0000

    add comment professeur

---
+++ forsaTaalim/app/Http/Controllers/AvisController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AvisRequests;
+use App\Models\driveer;
+use App\Services\AvisServices;
+use Illuminate\Http\Request;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
+
+class AvisController extends Controller
+{
+    protected $avisServices;
+    public function __construct(AvisServices $avisServices)
+    {
+        $this->avisServices = $avisServices;
+    }
+    public function poster(AvisRequests $request)
+    {
+        $valadetaData = $request->validated();
+        $this->avisServices->create($valadetaData);
+        return response()->JSON(['success', 'Post ajouté avec succès']);
+    }
+
+    public function delete($id)
+    {
+        $this->avisServices->delete($id);
+        return response()->json(['message' => 'comment deleted successfully!'], 200);
+    }
+
+    public function edit(Request $request, $id)
+    {
+        $commentsedite = $this->avisServices->update($id , $request->all());
+        return response()->json(['message' => 'comment updated successfully!', 'role' => $commentsedite], 200);
+    }
+    public function getById($id){
+        $comments = $this->avisServices->getById($id);
+        return response()->json(['message' => 'comment Ajoute successfully!', 'comments' => $comments], 200);
+    }
+
+    
+
+}
