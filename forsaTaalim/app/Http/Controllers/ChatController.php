8d73b21e6c3ece951ad8ba5c34e3ce2d093c24f3
MohamedMoustir <itsmoustir@gmail.com>
2025-04-03 13:45:41 +0000

    add style to page prancipale

--- forsaTaalim/app/Http/Controllers/ChatController.php
+++ forsaTaalim/app/Http/Controllers/ChatController.php
@@ -28,19 +28,30 @@ class ChatController extends Controller
     }
     public function getMessage($id)
     {
-
-        $chats = Chat::where(function ($query) use ($id) {
-            $query->where('sender_id', Auth::user()->id)->where('receiver_id', $id);
-        })->orWhere(function ($query) use ($id) {
-            $query->where('sender_id', $id)->where('receiver_id', Auth::user()->id);
-        })->orderBy('created_at', 'asc')->get();
-
+        $chats = DB::table('chats')
+        ->join('users as senders', 'chats.sender_id', '=', 'senders.id')
+        ->join('users as receivers', 'chats.receiver_id', '=', 'receivers.id')
+        ->select(
+            'chats.message',
+            'chats.created_at',
+            'senders.name as sender_name',
+            'senders.role as sender_role',
+            'receivers.name as receiver_name',
+            'receivers.role as receiver_role'
+        )
+        // ->where('chats.sender_id', '=', Auth::id())
+        // ->where('chats.receiver_id', '=', $id)
+        ->orderBy('chats.created_at', 'asc')
+        ->get();
+    
         $conversation = $chats->map(function ($chat) {
             return [
                 'message' => $chat->message,
-                'sender' => $chat->sender_id == Auth::id() ? Auth::user()->role : 'etudiant'
+                'sender' => $chat->sender_role,
+                'receiver' => $chat->receiver_role,
             ];
         });
+        
 
         return response()->json([
             'message' => $conversation,
