c69424b16208ec423e64bddbffd95f131513a799
MohamedMoustir <itsmoustir@gmail.com>
2025-04-03 22:20:40 +0000

    add fiche NotificationServiese pour line avec controller ou bas des donnes

---
+++ forsaTaalim/app/Services/NotificationServices.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\CategorieMatiere;
+use App\Models\Notification;
+use App\Models\Tag;
+use App\Repositories\NotificationRepositories;
+use App\Interface\CrudInterface;
+use App\Repositories\AuthRepository;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Termwind\Components\Dd;
+
+class NotificationServices implements CrudInterface
+{
+    protected $notificationRepositories;
+
+    public function __construct(NotificationRepositories $notificationRepositories)
+    {
+        $this->notificationRepositories = $notificationRepositories;
+    }
+    public function create(array $data)
+    {
+      
+        $data['sender_id'] = Auth::id();
+       
+        return $this->notificationRepositories->create($data);
+    }
+    public function getAll(){}
+    public function getById($id)
+    {
+        return Notification::findOrFail($id,'receiver_id');
+    }
+    public function update($id, array $data){}
+    public function delete($id)
+    {
+        return $this->notificationRepositories->delete($id);
+    }
+}
