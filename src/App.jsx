afb1d067c47cc0aa130dc9605df1da51ec7877cd
MohamedMoustir <itsmoustir@gmail.com>
2025-04-10 15:55:19 +0100

    add routs home

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import Login from "./components/Auth/Login";
-import Rejister from "./components/Auth/Rejister";
-import Chat from "./components/Chats/Chats";
-import RejisterPro from "./components/Auth/RejisterPro";
-import Index from "./components/index";
+import Login from "./Auth/Login";
+import Rejister from "./Auth/Rejister";
+import Chat from "./Chats/Chats";
+import RejisterPro from "./Auth/RejisterPro";
+import Index from "./pages/index";
+import '../../forsaTaalim-front/src/assets/js/index';
+import Tutors from "./pages/tutors"
 
 
 
 function App() {
   return (
     <Router>
-      <nav>
+      {/* <nav className="flex">
         <ul>
           <li><Link to="/index"></Link></li>
           <li><Link to="/login">login</Link></li>
@@ -20,14 +22,17 @@ function App() {
           <li><Link to="/Rejister">Rejister</Link></li>
           <li><Link to="/Chat">Chat</Link></li>
         </ul>
-      </nav>
+      </nav> */}
 
       <Routes>
-      <Route path="/login" element={<Index />} />
+
+      <Route path="/home" element={<Index />} />
         <Route path="/login" element={<Login />} />
         <Route path="/RejisterPro" element={<RejisterPro />} />
         <Route path="/Rejister" element={<Rejister />} />
         <Route path="/Chat" element={<Chat />} />
+        <Route path="/tutors" element={<Tutors />} />
+
       </Routes>
     </Router>
   );
