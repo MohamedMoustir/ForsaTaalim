8fdcba84bcb919d89f9291a5a01397a416a25727
MohamedMoustir <itsmoustir@gmail.com>
2025-04-12 18:12:44 +0100

    Développement du repository pour les fonctionnalités de chat

--- forsaTaalim/app/Repositories/ChatRepositories.php
+++ forsaTaalim/app/Repositories/ChatRepositories.php
@@ -35,7 +35,6 @@ class ChatRepositories
             ]
         );
        
-        // Trigger the Pusher event with current timestamp
         $pusher->trigger('forsaTaalm-development', 'message', [
             'message' => $data['message'],
             'sender' => Auth::user()->role,
