19e3eb58ebcc68af6ea75f28e0aef97f7332fc5e
MohamedMoustir <itsmoustir@gmail.com>
2025-04-12 18:12:45 +0100

    Implémentation du repository pour la gestion des professeurs

--- forsaTaalim/app/Repositories/ProfesseurRepositories.php
+++ forsaTaalim/app/Repositories/ProfesseurRepositories.php
@@ -21,6 +21,7 @@ class ProfesseurRepositories
     }
     public function create($data)
     {
+       
         return $this->model->create($data);
     }
     public function update($id, $data)
@@ -42,10 +43,17 @@ class ProfesseurRepositories
         $showProfile = DB::table('professeurs as p')
             ->join('users as u', 'p.user_id', '=', 'u.id')
             ->join('categorie_matieres as c', 'p.categorieMatiere_id', '=', 'c.id')
-            ->leftjoin('competence_professeurs as cp', 'u.id', '=', 'cp.professeur_id')
-            ->leftjoin('competences as com', 'cp.competence_id', '=', 'com.id')
+            ->leftJoin('competence_professeurs as cp', 'u.id', '=', 'cp.professeur_id')
+            ->leftJoin('competences as com', 'cp.competence_id', '=', 'com.id')
+            ->leftJoin('comments as comme1', 'u.id', '=', 'comme1.tuteur_id')
             ->select(
-                'p.*',
+                'p.id',
+                'p.diplomes',
+                'p.experiences',
+                'p.tarifHoraire',
+                'p.location',
+                'p.biographie',
+                'p.video',
                 'u.prenom',
                 'u.email',
                 'u.photo',
@@ -56,100 +64,54 @@ class ProfesseurRepositories
                 'u.password',
                 'u.remember_token',
                 'c.nom as nom_matiere',
-                'com.name'
+                'com.name as competencesName',
+                'u.id as profe_id',
+                DB::raw('COUNT(comme1.rating) as total_ratings')
+            )->groupBy(
+                'p.id',
+                'p.diplomes',
+                'p.experiences',
+                'p.tarifHoraire',
+                'p.location',
+                'p.biographie',
+                'p.video',
+                'u.prenom',
+                'u.email',
+                'u.photo',
+                'u.age',
+                'u.telephone',
+                'u.role',
+                'u.email_verified_at',
+                'u.password',
+                'u.remember_token',
+                'c.nom',
+                'com.name',
+                'profe_id'
+
             )
-            ->where('u.id', '=', $id)
-            ->get();
+            ->where('p.id', '=', $id)
+            ->first();
 
         return $showProfile;
     }
     // you want test
-    public function getAll($data)
+    public function getAll()
     {
 
         $Tuteur = DB::table('professeurs as p')
             ->join('users as u', 'p.user_id', '=', 'u.id')
             ->join('categorie_matieres as c', 'p.categorieMatiere_id', '=', 'c.id')
-            ->leftjoin('competence_professeurs as cp', 'u.id', '=', 'cp.professeur_id')
-            ->leftjoin('competences as com', 'cp.competence_id', '=', 'com.id')
+            ->leftJoin('competence_professeurs as cp', 'u.id', '=', 'cp.professeur_id')
+            ->leftJoin('competences as com', 'cp.competence_id', '=', 'com.id')
+            ->leftJoin('comments as comme1', 'u.id', '=', 'comme1.tuteur_id')
             ->select(
-           'p.id',
-            'p.diplomes',
-            'p.experiences',
-            'p.tarifHoraire',
-            'p.disponible',
-            'p.location',
-            'p.biographie',
-            'p.video',
-            'u.prenom',
-            'u.email',
-            'u.photo',
-            'u.age',
-            'u.telephone',
-            'u.role',
-            'u.email_verified_at',
-            'u.password',
-            'u.remember_token',
-            'c.nom as nom_matiere',
-            'com.name as competencesName'
-            );
-
-        if ($data['search'] || $data['sear']) {
-            $search = $data['search'];
-            $sear = $data['sear'];
-            $Tuteur->whereHas('professeurs', function ($query) use ($search) {
-                $query->where('tarifHoraire', 'ILIKE', "%{$search}%");
-            });
-
-            $trajets->whereHas('driveer', function ($query) use ($sear) {
-                $query->where('disponible', 'ILIKE', "%{$sear}%");
-            });
-        }
-
-
-
-    }
-    public function getAllwithout(){
-        return  DB::table('professeurs as p')
-        ->join('users as u', 'p.user_id', '=', 'u.id')
-        ->join('categorie_matieres as c', 'p.categorieMatiere_id', '=', 'c.id')
-        ->leftjoin('competence_professeurs as cp', 'u.id', '=', 'cp.professeur_id')
-        ->leftjoin('competences as com', 'cp.competence_id', '=', 'com.id')
-        ->leftjoin('comments as comme1',  'u.id', '=', 'comme1.tuteur_id')
-        ->select(
-            'p.id',
-            'p.diplomes',
-            'p.experiences',
-            'p.tarifHoraire',
-            'p.disponible',
-            'p.location',
-            'p.biographie',
-            'p.video',
-            'u.prenom',
-            'u.email',
-            'u.photo',
-            'u.age',
-            'u.telephone',
-            'u.role',
-            'u.email_verified_at',
-            'u.password',
-            'u.remember_token',
-            'c.nom as nom_matiere',
-            'com.name as competencesName',
-            DB::COUNT('comme1.rating') ,
-        )
-        ->get();
-    }
-    public function filter($value)
-    {
-        $filterPar = DB::table('professeurs as p')
-            ->join('users as u', 'p.user_id', '=', 'u.id')
-            ->join('categorie_matieres as c', 'p.categorieMatiere_id', '=', 'c.id')
-            ->leftjoin('competence_professeurs as cp', 'u.id', '=', 'cp.professeur_id')
-            ->leftjoin('competences as com', 'cp.competence_id', '=', 'com.id')
-            ->where('com.name', 'like', '%', $value, '%')
-            ->select(
-                'p.*',
+                'p.id',
+                'p.diplomes',
+                'p.experiences',
+                'p.tarifHoraire',
+                'p.location',
+                'p.biographie',
+                'p.video',
                 'u.prenom',
                 'u.email',
                 'u.photo',
@@ -160,8 +122,151 @@ class ProfesseurRepositories
                 'u.password',
                 'u.remember_token',
                 'c.nom as nom_matiere',
-                'com.name'
-            )->paginate(6);
+                'com.name as competencesName',
+                'u.id as profe_id',
+                DB::raw('COUNT(comme1.rating) as total_ratings')
+            )
+            ->groupBy(
+                'p.id',
+                'p.diplomes',
+                'p.experiences',
+                'p.tarifHoraire',
+                'p.location',
+                'p.biographie',
+                'p.video',
+                'u.prenom',
+                'u.email',
+                'u.photo',
+                'u.age',
+                'u.telephone',
+                'u.role',
+                'u.email_verified_at',
+                'u.password',
+                'u.remember_token',
+                'c.nom',
+                'com.name',
+                'profe_id'
+            )
+            ->get();
+
+        // if ($data['search'] || $data['sear']) {
+        //     $search = $data['search'];
+        //     $sear = $data['sear'];
+        //     $Tuteur->whereHas('professeurs', function ($query) use ($search) {
+        //         $query->where('tarifHoraire', 'ILIKE', "%{$search}%");
+        //     });
+
+        //     $trajets->whereHas('driveer', function ($query) use ($sear) {
+        //         $query->where('disponible', 'ILIKE', "%{$sear}%");
+        //     });
+        // }
+
+
+
     }
+    public function getAllwithout()
+    {
+        return DB::table('professeurs as p')
+            ->join('users as u', 'p.user_id', '=', 'u.id')
+            ->join('categorie_matieres as c', 'p.categorieMatiere_id', '=', 'c.id')
+            ->leftJoin('competence_professeurs as cp', 'u.id', '=', 'cp.professeur_id')
+            ->leftJoin('competences as com', 'cp.competence_id', '=', 'com.id')
+            ->leftJoin('comments as comme1', 'u.id', '=', 'comme1.tuteur_id')
+            ->select(
+                'p.id',
+                'p.diplomes',
+                'p.experiences',
+                'p.tarifHoraire',
+                'p.location',
+                'p.biographie',
+                'p.video',
+                'u.prenom',
+                'u.email',
+                'u.photo',
+                'u.age',
+                'u.telephone',
+                'u.role',
+                'u.email_verified_at',
+                'u.password',
+                'u.remember_token',
+                'c.nom as nom_matiere',
+                'com.name as competencesName',
+                'u.id as profe_id',
+                DB::raw('COUNT(comme1.rating) as total_ratings')
+            )
+            ->groupBy(
+                'p.id',
+                'p.diplomes',
+                'p.experiences',
+                'p.tarifHoraire',
+                'p.location',
+                'p.biographie',
+                'p.video',
+                'u.prenom',
+                'u.email',
+                'u.photo',
+                'u.age',
+                'u.telephone',
+                'u.role',
+                'u.email_verified_at',
+                'u.password',
+                'u.remember_token',
+                'c.nom',
+                'com.name',
+                'profe_id',
+            )
+            ->paginate(6);
+    }
+
+    // public function filter($value)
+    // {
+    //     $filterPar = DB::table('professeurs as p')
+    //         ->join('users as u', 'p.user_id', '=', 'u.id')
+    //         ->join('categorie_matieres as c', 'p.categorieMatiere_id', '=', 'c.id')
+    //         ->leftJoin('competence_professeurs as cp', 'u.id', '=', 'cp.professeur_id')
+    //         ->leftJoin('competences as com', 'cp.competence_id', '=', 'com.id')
+    //         ->leftJoin('comments as comme1', 'u.id', '=', 'comme1.tuteur_id')
+    //         ->select(
+    //             'p.id',
+    //             'p.diplomes',
+    //             'p.experiences',
+    //             'p.tarifHoraire',
+    //             'p.location',
+    //             'p.biographie',
+    //             'p.video',
+    //             'u.prenom',
+    //             'u.email',
+    //             'u.photo',
+    //             'u.age',
+    //             'u.telephone',
+    //             'u.role',
+    //             'u.email_verified_at',
+    //             'u.password',
+    //             'u.remember_token',
+    //             'c.nom as nom_matiere',
+    //             'com.name as competencesName',
+    //             DB::raw('COUNT(comme1.rating) as total_ratings')
+    //         )
+    //         ->groupBy(
+    //             'p.id',
+    //             'p.diplomes',
+    //             'p.experiences',
+    //             'p.tarifHoraire',
+    //             'p.location',
+    //             'p.biographie',
+    //             'p.video',
+    //             'u.prenom',
+    //             'u.email',
+    //             'u.photo',
+    //             'u.age',
+    //             'u.telephone',
+    //             'u.role',
+    //             'u.email_verified_at',
+    //             'u.password',
+    //             'u.remember_token',
+    //             'c.nom',
+    //             'com.name'
+    //         )->paginate(6);
+    // }
 
 }
