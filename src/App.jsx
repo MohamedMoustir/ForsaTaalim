5ea143248209553512cffdf87b8aed4fb156271e
MohamedMoustir <itsmoustir@gmail.com>
2025-04-13 15:39:08 +0100

    update: modify main application routes and layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ import Tutors from "./pages/tutors"
 import DetilesTutor from "./pages/detilesTutor"
 import ContactTutors from "./pages/contactTutors"
 import Reservation from "./pages/reservation"
-
-
-
+import Profile from "./pages/profile"
+import Myfavorites from "./pages/Myfavorites"
+import Disponibilites from "./Professeur/Disponibilites"
 function App() {
   return (
     <Router>
@@ -29,18 +29,18 @@ function App() {
 
       <Routes>
 
-      <Route path="/home" element={<Index />} />
+      <Route path="/" element={<Index />} />
         <Route path="/login" element={<Login />} />
         <Route path="/RejisterPro" element={<RejisterPro />} />
         <Route path="/Rejister" element={<Rejister />} />
-        <Route path="/Chat" element={<Chat />} />
+        <Route path="/Chat/:id" element={<Chat />} />
         <Route path="/tutors" element={<Tutors />} />
         <Route path="/detilesTutor/:id" element={<DetilesTutor />} />
         <Route path="/contactTutors/:id" element={<ContactTutors />} />
         <Route path="/reservation/:id" element={<Reservation />} />
-
-
-
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/favorites" element={<Myfavorites />} />
+        <Route path="/disponibilites/:id" element={<Disponibilites />} />
 
 
       </Routes>
