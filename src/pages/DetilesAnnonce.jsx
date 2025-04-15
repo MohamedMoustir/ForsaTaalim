7a1cc507f64b86f58cc90248419d43a270e7e219
MohamedMoustir <itsmoustir@gmail.com>
2025-04-15 15:55:03 +0100

    commence les page detiles des annonce

---
+++ src/pages/DetilesAnnonce.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { 
+  faCalendarAlt, 
+  faMapMarkerAlt, 
+  faChalkboardTeacher, 
+  faBookOpen, 
+  faUsers, 
+  faSignInAlt,
+  faClock,
+  faTicketAlt,
+  faShareAlt,
+  faHeart,
+  faBookmark
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
