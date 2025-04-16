653224038329af54a742387ef114751cd7e3edaa
MohamedMoustir <itsmoustir@gmail.com>
2025-04-16 22:47:31 +0100

    add propr to function create in interface

--- forsaTaalim/app/Interface/CrudInterface.php
+++ forsaTaalim/app/Interface/CrudInterface.php
@@ -3,7 +3,7 @@ namespace App\Interface;
 
 interface CrudInterface
 {
-    public function create(array $data);
+    public function create(array $data );
     public function getAll();
     public function getById($id);
     public function update($id, array $data);
