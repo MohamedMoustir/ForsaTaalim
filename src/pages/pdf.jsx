a6a05e1a70c8f71d31434ec6efc2dc6260191260
MohamedMoustir <itsmoustir@gmail.com>
2025-04-18 23:51:59 +0100

    commense add view pdf paymen

---
+++ src/pages/pdf.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 30,
+    backgroundColor: '#f8f8f8',
+  },
+  section: {
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  label: {
+    fontSize: 14,
+    marginBottom: 4,
+  },
+});
+ 
+const MyDocument = ({Mespaiements}) => {
+
+
+ 
+};
+
+export default MyDocument;
