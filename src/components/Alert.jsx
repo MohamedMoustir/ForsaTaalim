950e2c70b05c16b8ae0f321816f6d299e3019ee9
MohamedMoustir <itsmoustir@gmail.com>
2025-04-18 09:36:04 +0100

    seipartion les alert por organisation des code

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Alert = ({ type = 'success', title, message, onClose }) => {
   const baseStyle = {
-    success: 'bg-green-100 border-l-4 border-green-500 text-green-700',
+    success: 'bg-green-100 border-l-4 border-green-500 text-green-700 ',
     error: 'bg-red-100 border-l-4 border-red-500 text-red-700',
     warning: 'bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700',
     info: 'bg-blue-100 border-l-4 border-blue-500 text-blue-700',
   };
 
   return (
-    <div className={`relative p-4 rounded-lg ${baseStyle[type]} max-w-xl mx-auto my-4`}>
+    <div className={`absolute p-4 rounded-lg ${baseStyle[type]} max-w-xl mx-auto my-4 mr-8 right-0  `}>
       {onClose && (
         <button
           onClick={onClose}
