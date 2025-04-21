63312f734f57ad14149325a0f09f73c9c866ad48
MohamedMoustir <itsmoustir@gmail.com>
2025-04-21 10:13:27 +0100

    add const logout pour remove token in session storge and auth

---
+++ src/Auth/logout.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState  } from "react";
+import axios from "axios";
+import { API_URL, getToken } from "../utils/config";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
+const Logout = () => {
+    const token = getToken();
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const handleLogout = async () => {
+            const response = await axios.post(`${API_URL}/auth/logout`, {},
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "multipart/form-data",
+                    }
+                }
+            );
+
+            if (response) {
+                sessionStorage.removeItem("user");
+                sessionStorage.removeItem("token");
+                navigate('/login')
+                setLoading(false)
+            }
+
+
+        }
+        handleLogout()
+        console.log('ddddd');
+    }, [])
+
+    return (
+        <>  {loading && <Spinner />}</>
+    )
+
+}
+export default Logout;
