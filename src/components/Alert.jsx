364782254cabbc3642cc40aff70b5a30411954c5
MohamedMoustir <itsmoustir@gmail.com>
2025-04-24 09:05:38 +0100

    style card pop up

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import "../assets/style/style.css"
+import "../assets/style/style.css";
+
 const Alert = ({ type = 'success', title, message, onClose }) => {
-
   return (
-    <div class={`flex absolute  items-center justify-between gap-4 px-4 py-3 rounded-lg shadow-lg min-w-[300px] max-w-[400px] border-l-4 max-w-xl mx-auto my-4 mr-8 right-0 
-      ${type === 'success' ? 'bg-white border-green-500' : ''}
-      ${type === 'error' ? 'bg-white border-red-500 ' : ''}
-      ${type === 'warning' ? 'bg-white border-yellow-500' : ''}
-    `} style={{ zIndex: 1 ,fontFamily: 'Open Sans'}} >
-      <i class={`text-xl 
-        ${type === 'success' ? 'text-green-500' : ''}
-        ${type === 'error' ? 'text-red-500' : ''}
-        ${type === 'warning' ? 'text-yellow-500' : ''}
-        fa-solid fa-check-circle`}></i>
+    <div
+      className={`flex absolute items-center justify-between gap-4 px-4 py-3 rounded-lg shadow-lg min-w-[300px] max-w-[400px] border-l-4 max-w-xl mx-auto my-4 mr-8 right-0
+        ${type === 'success' ? 'bg-white border-green-500' : ''}
+        ${type === 'error' ? 'bg-white border-red-500' : ''}
+        ${type === 'warning' ? 'bg-white border-yellow-500' : ''}
+      `}
+      style={{ zIndex: 1, fontFamily: 'Open Sans' }}
+    >
+      <i
+        className={`text-xl
+          ${type === 'success' ? 'text-green-500' : ''}
+          ${type === 'error' ? 'text-red-500' : ''}
+          ${type === 'warning' ? 'text-yellow-500' : ''}
+          fa-solid fa-check-circle`}
+      ></i>
 
-      <div class="flex-1 ">
-        <div class="font-semibold text-gray-800">{title}</div>
-        <span class="text-sm text-gray-600">{message}</span>
+      <div className="flex-1">
+        <div className="font-semibold text-gray-800">{title}</div>
+        <span className="text-sm text-gray-600">{message}</span>
       </div>
 
-      <i onClick={onClose} class="fa-solid fa-xmark text-gray-400 hover:text-black cursor-pointer"></i>
+      <i
+        onClick={onClose}
+        className="fa-solid fa-xmark text-gray-400 hover:text-black cursor-pointer"
+      ></i>
     </div>
-
   );
 };
 
